28474c7b2dde95bb2a02c81d8180013125681026
Andrey Kraveckiy <andreykraveckiy@gmail.com>
2016-10-24 14:24:22 +0300

    Initialize post

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WallPosts::Application.config.secret_key_base = '3c69c37c3514214d89710ccdf9ce43044f470aa31495e1a505705637f174ae9c6f86c71caf670f4508f04d24f2e8812cc0fb2e9a4d81de85700aaa6cbcb56c3d'
