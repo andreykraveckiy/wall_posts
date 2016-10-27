b93ee38699186edaefc3c18d029a9f66a7a4d6c7
Andrey Kraveckiy <andreykraveckiy@gmail.com>
2016-10-27 18:42:38 +0300

    Hide field for text in each comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,17 @@
 class CommentsController < ApplicationController
-	before_action :parent_post, only: [:create]
+	before_action :parent_object, only: [:new, :create]
+	before_action :set_id, only: [:new, :create]
+
+	def new
+		@comment = Comment.new
+		respond_to do |format|
+			format.html { redirect_to posts_url }
+			format.js
+		end
+	end
 
 	def create	
 		@comment = Comment.new(comment_params)
-		@id = "#{params[:commentable_type]}#{params[:commentable_id]}"
 		if @comment.save
 			flash[:success] = "Comment created!"
 		end
@@ -19,11 +27,15 @@ class CommentsController < ApplicationController
 			params.permit(:content, :commentable_id, :commentable_type, :user_id)
 		end
 
-		def parent_post			
+		def parent_object			
 			if params[:commentable_type].eql?("Post")
-				@post = Post.find(params[:commentable_id])
+				@object = Post.find(params[:commentable_id])
 			else
-				@post = Comment.find(params[:commentable_id])
+				@object = Comment.find(params[:commentable_id])
 			end
+		end
+
+		def set_id
+			@id = "#{params[:commentable_type]}#{params[:commentable_id]}"
 		end
 end
