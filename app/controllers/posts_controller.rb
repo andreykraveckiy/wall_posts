1ca38640c75404c8881d6e7546c3b0a0c923d25d
Andrey Kraveckiy <andreykraveckiy@gmail.com>
2016-10-28 14:52:16 +0300

    Update for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,30 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-  before_action :all_posts, only: [:index, :create, :destroy]
+  before_action :all_posts, only: [:index, :create, :update, :destroy]
+  before_action :set_own_id, only: [:edit]
 
   def create
     @post = current_user.posts.build(post_params) if user_signed_in?
     @post.save
     respond_to do |format|
       format.html { render 'index' }
+      format.js
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    respond_to do |format|
+      format.html { redirect_to posts_url }
+      format.js
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(content: params[:content])
+    respond_to do |format|
+      format.html { redirect_to posts_url }
       format.js
     end
   end
@@ -28,5 +46,9 @@ class PostsController < ApplicationController
 
     def all_posts
       @posts = Post.all
+    end
+
+    def set_own_id
+      @own_id = "Post#{params[:id]}"
     end
 end
